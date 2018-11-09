a93a41d09da0931376e5bfcaf4b0061fc08e9070
xuebao <luxuebao@126.com>
2018-11-09 19:47:39 +0800

    update

--- src/gui/frame/MainFrame.java
+++ src/gui/frame/MainFrame.java
@@ -1,7 +1,13 @@
 package gui.frame;
 
+import javax.swing.*;
+
 /**
  * Created by Administrator on 2018/11/9.
  */
-public class MainFrame {
+public class MainFrame extends JFrame {
+    private MainFrame(){
+
+    }
+    public static MainFrame instance = new MainFrame();
 }
--- src/gui/panel/MainPanel.java
+++ src/gui/panel/MainPanel.java
@@ -4,4 +4,7 @@ package gui.panel;
  * Created by Administrator on 2018/11/9.
  */
 public class MainPanel {
+    private MainPanel(){
+
+    }
 }
